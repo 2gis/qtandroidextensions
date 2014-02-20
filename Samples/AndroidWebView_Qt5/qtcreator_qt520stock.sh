122bf41277cc483e180a65db41e0b809ee3ef41b
Галин Сергей Александрович <sergey.galin@gmail.com>
2014-02-20 16:44:33 +0700

    Replace Qt Creator run script

--- Samples/AndroidWebView_Qt5/qtcreator_qt520stock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-"$HOME/Qt5.2.0-Android/Tools/QtCreator/bin/qtcreator.sh" *.pro 2>&1 > /dev/null &
-
---
+++ Samples/AndroidWebView_Qt5/qtcreator_qt521stock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"$HOME/Qt5.2.1/Tools/QtCreator/bin/qtcreator.sh" *.pro 2>&1 > /dev/null &
