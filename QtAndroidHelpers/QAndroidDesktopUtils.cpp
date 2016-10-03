ee1dc41ee7549bdf4e0bff39b0ead6710aa60993
Галин Сергей Александрович <sergey.galin@gmail.com>
2016-10-03 15:14:11 +0700

    Removed exception in getDefaultLocaleName()

--- QtAndroidHelpers/QAndroidDesktopUtils.cpp
+++ QtAndroidHelpers/QAndroidDesktopUtils.cpp
@@ -13,13 +13,13 @@
   modification, are permitted provided that the following conditions are met:
 
   * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
+	this list of conditions and the following disclaimer.
   * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
   * Neither the name of the DoubleGIS, LLC nor the names of its contributors
-    may be used to endorse or promote products derived from this software
-    without specific prior written permission.
+	may be used to endorse or promote products derived from this software
+	without specific prior written permission.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -291,7 +291,15 @@ QString getUniqueDeviceId()
 QString getDefaultLocaleName()
 {
 	QJniClass du(c_full_class_name_);
-	return du.callStaticString("getDefaultLocaleName");
+	if (du.jClass())
+	{
+		return du.callStaticString("getDefaultLocaleName");
+	}
+	else
+	{
+		qCritical() << "Null class:" << c_full_class_name_;
+		return QLatin1String("C");
+	}
 }
 
 bool checkSelfPermission(const QString & permission_name)
