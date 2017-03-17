1a3d273424dd11ccd172799e4c326ff8eef4dc7d
Sergey Lagner <s.lagner@2gis.ru>
2017-03-17 12:28:39 +0700

    qt 5.8 and 5.9 private headers

--- QtAndroidAssets/AndroidAssetsFileEngineHandler_p.h
+++ QtAndroidAssets/AndroidAssetsFileEngineHandler_p.h
@@ -31,6 +31,10 @@
 #   include <QtCore/5.6.1/QtCore/private/qabstractfileengine_p.h>
 #elif QTCORE_VERSION == 0x050602
 #   include <QtCore/5.6.2/QtCore/private/qabstractfileengine_p.h>
+#elif QTCORE_VERSION == 0x050800
+#   include <QtCore/5.8.0/QtCore/private/qabstractfileengine_p.h>
+#elif QTCORE_VERSION == 0x050900
+#   include <QtCore/5.9.0/QtCore/private/qabstractfileengine_p.h>
 #else
 #   error qt version unknown
 #endif
