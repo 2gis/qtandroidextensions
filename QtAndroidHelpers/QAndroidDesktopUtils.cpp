16d7eec207034556b2a78230866aca790c401372
M. Karapetyan <m.karapetyan@2Gis.ru>
2021-09-13 10:39:54 +0300

    fix whitespase

--- QtAndroidHelpers/QAndroidDesktopUtils.cpp
+++ QtAndroidHelpers/QAndroidDesktopUtils.cpp
@@ -556,9 +556,9 @@ bool isBluetoothLEAvailable()
 	if (du.jClass())
 	{
 		return du.callStaticParamBoolean(
-		    "isBluetoothLEAvailable",
-		    "Landroid/content/Context;",
-		    QAndroidQPAPluginGap::Context().jObject());
+			"isBluetoothLEAvailable",
+			"Landroid/content/Context;",
+			QAndroidQPAPluginGap::Context().jObject());
 	}
 	else
 	{
@@ -571,33 +571,33 @@ bool zipFiles(const QStringList & src_paths, const QString & dst_path)
 {
 	QJniClass du(c_full_class_name_);
 	if (du.jClass())
-        {
-            QJniEnvPtr jep;
+	{
+		QJniEnvPtr jep;
 
-            QJniLocalRef src_paths_array(jep.env()->NewObjectArray(
-                src_paths.size(),
-                QJniClass("java/lang/String").jClass(),
-                nullptr));
+		QJniLocalRef src_paths_array(jep.env()->NewObjectArray(
+			src_paths.size(),
+			QJniClass("java/lang/String").jClass(),
+			nullptr));
 
-            for (QStringList::size_type i = 0; i < src_paths.size(); ++i)
-            {
-                    jep.env()->SetObjectArrayElement(
-                        static_cast<jobjectArray>(src_paths_array.jObject()),
-                        i,
-                        QJniLocalRef(src_paths[i]).jObject());
-            }
+		for (QStringList::size_type i = 0; i < src_paths.size(); ++i)
+		{
+				jep.env()->SetObjectArrayElement(
+					static_cast<jobjectArray>(src_paths_array.jObject()),
+					i,
+					QJniLocalRef(src_paths[i]).jObject());
+		}
 
-            return du.callStaticParamBoolean(
-                "zipFiles",
-                "[Ljava/lang/String;Ljava/lang/String;",
-                static_cast<jobjectArray>(src_paths_array.jObject()),
-                QJniLocalRef(dst_path).jObject());
+		return du.callStaticParamBoolean(
+			"zipFiles",
+			"[Ljava/lang/String;Ljava/lang/String;",
+			static_cast<jobjectArray>(src_paths_array.jObject()),
+			QJniLocalRef(dst_path).jObject());
 	}
 	else
 	{
 		qCritical() << "Null class:" << c_full_class_name_;
 		return false;
-        }
+	}
 }
 
 } // namespace QAndroidDesktopUtils
